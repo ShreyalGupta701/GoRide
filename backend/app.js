7e6e3c92dd0fc35b39179aa1eb890b2796e81041
ShreyalGupta701 <your-shreyalg70@gmail.com>
2026-05-25 19:48:04 +0530

    user registration done

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,15 @@ const cors = require('cors');
 
 const app = express();
 
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/users', userRoutes);
+
+connectToDb();
 app.use(cors());
 
 app.get('/', (req, res) => {
